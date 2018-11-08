9fee3653bb83657546eb1863f8021a5638c8599d
Kate Dameron <katerj33@gmail.com>
2018-11-07 19:42:14 -0800

    added the Handlebars template and the compiler

--- starter-code/public/scripts/articleView.js
+++ starter-code/public/scripts/articleView.js
@@ -2,7 +2,7 @@
 
 // TODO: Wrap the entire contents of this file in an IIFE.
 // Pass in to the IIFE a module, upon which objects can be attached for later access.
-// (function(module){
+(function(module){
 
 var articleView = {};
 
@@ -130,6 +130,7 @@ articleView.initIndexPage = function() {
 };
 
 articleView.initAdminPage = function() {
+  var template = Handlebars.compile($('#author-template').html() );
   // TODO: Call the Handlebars `.compile` function, which will return a function for you to use where needed.
   // Make sure you assign the result of your Handlebars.compile call to a variable called "template", since
   // we are then calling "template" on line 117.
@@ -143,4 +144,4 @@ articleView.initAdminPage = function() {
   $('#blog-stats .articles').text(Article.all.length);
   $('#blog-stats .words').text(Article.numWordsAll());
 };
-// }(window));
+})(window);
